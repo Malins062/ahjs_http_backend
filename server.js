85584cd674d1bd1b835ce483773185109907eea5
Malins062 <6328673@gmail.com>
2022-12-07 11:32:55 +0300

    Some changes

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+
 const http = require('http');
 const Koa = require('koa');
+const koaBody = require('koa-body');
+const app = new Koa();
 
-const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  res.end('Server response');
+const tickets = [];
+
+app.use(koaBody({
+  urlencoded: true,
+}));
+
+app.use(async ctx => {
+    const { method } = ctx.request.querystring;
+
+    switch (method) {
+        case 'allTickets':
+            ctx.response.body = tickets;
+            return;
+        // TODO: обработка остальных методов
+        default:
+            ctx.response.status = 404;
+            return;
+    }
 });
 
 const port = 7070;
