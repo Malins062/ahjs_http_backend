efe9578364e0228f1aed233ce8eaefa4de458e33
Malins062 <6328673@gmail.com>
2023-01-05 12:27:54 +0300

    Eslint editing errors

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const Koa = require('koa');
 const koaBody = require('koa-body');
 const koaStatic = require('koa-static');
 
+function nowDateTime() {
+  const now = new Date();
+  const result = `${now.toLocaleString([],
+    {
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    })}`;
+
+  return result;
+}
+
 const app = new Koa();
 
 const tickets = [
@@ -13,34 +28,34 @@ const tickets = [
     name: 'Поменять краску в принтере, каб.404',
     description: 'Закончилась краска в принтере, модель Epson-CJ300',
     status: true,
-    created: '10.03.2022 08:40'
+    created: '10.03.2022 08:40',
   },
   {
     id: '2',
     name: 'Переустановить OC Linux, каб.204',
     description: 'Слетела операционная система, требуется переустановка',
     status: true,
-    created: '22.11.2022 13:10'
+    created: '22.11.2022 13:10',
   },
   {
     id: '3',
     name: 'Установить обновление KB-32565, каб.106',
     description: 'Вышло критическое обновление Windows-10, требуется установка обновления',
     status: false,
-    created: '08.12.2022 10:00'
+    created: '08.12.2022 10:00',
   },
   {
     id: '4',
     name: 'Задача4',
     description: 'Описание задачи',
     status: false,
-    created: '08.01.2023 10:00'
+    created: '08.01.2023 10:00',
   },
 ];
 
 // => Static file handling
-const public = path.join(__dirname, '/public')
-app.use(koaStatic(public));
+const publics = path.join(__dirname, '/public');
+app.use(koaStatic(publics));
 
 app.use(koaBody({
   urlencoded: true,
@@ -68,86 +83,84 @@ app.use(async (ctx, next) => {
   }
 
   const { method } = ctx.query;
-  console.log(ctx.request, ctx.query, ctx.request.body);
+  // console.log(ctx.request, ctx.query, ctx.request.body);
 
   ctx.response.set('Access-Control-Allow-Origin', '*');
 
   switch (method) {
-      case 'createTicket':
-        const { name, description } = ctx.request.body;
+    case 'createTicket': {
+      const { name, description } = ctx.request.body;
 
-        if (tickets.some(ticket => ticket.name == name)) {
-          ctx.response.status = 400
-          ctx.response.body = `Задача с именем - "${name}" уже существует!`;
-          return;
-        }
-
-        const newTicket = {
-          id: uuid.v4(),
-          name: name,
-          description: description,
-          status: false,
-          created: nowDateTime()
-        }
-
-        tickets.push(newTicket);
-
-        ctx.response.body = newTicket;
-        ctx.response.status = 202;
-        console.log(newTicket);
+      if (tickets.some((ticket) => ticket.name === name)) {
+        ctx.response.status = 400;
+        ctx.response.body = `Задача с именем - "${name}" уже существует!`;
         return;
+      }
 
-      default:
-          ctx.response.status = 404;
-          return;
+      const newTicket = {
+        id: uuid.v4(),
+        name,
+        description,
+        status: false,
+        created: nowDateTime(),
+      };
+
+      tickets.push(newTicket);
+
+      ctx.response.body = newTicket;
+      ctx.response.status = 202;
+      // console.log(newTicket);  // eslint-disable-line no-console
+      return;
+    }
+
+    default:
+      ctx.response.status = 404;
   }
-
 });
 
 // => GET
-app.use(async ctx => {
+app.use(async (ctx) => {
   if (ctx.request.method !== 'GET') {
     return;
   }
   const { method } = ctx.query;
-  console.log(ctx.request, ctx.query, ctx.request.body);
+  // console.log(ctx.request, ctx.query, ctx.request.body);
 
   ctx.response.set('Access-Control-Allow-Origin', '*');
 
   switch (method) {
-      case 'allTickets':
-        ctx.response.status = 202;
-        const allTickets = tickets.map(item => {
-          return {
-            id: item.id,
-            name: item.name,
-            status: item.status,
-            created: item.created
-          }
-        });
+    case 'allTickets': {
+      ctx.response.status = 202;
+      const allTickets = tickets.map((item) => ({
+        id: item.id,
+        name: item.name,
+        status: item.status,
+        created: item.created,
+      }));
 
-        ctx.response.body = allTickets;
-        console.log(allTickets);
+      ctx.response.body = allTickets;
+      // console.log(allTickets);  // eslint-disable-line no-console
+      return;
+    }
+
+    case 'ticketById': {
+      const { id } = ctx.query;
+
+      if (tickets.every((item) => item.id !== id)) {
+        ctx.response.status = 400;
+        ctx.response.body = `Задачи с идентификатором ${id} не существует!`;
         return;
+      }
 
-      case 'ticketById':
-        const { id } = ctx.query;
+      const ticket = tickets.filter((item) => item.id === id);
+      ctx.response.body = ticket;
+      ctx.response.status = 202;
+      // console.log(ticket);  // eslint-disable-line no-console
+      return;
+    }
 
-        if (tickets.every(item => item.id !== id)) {
-          ctx.response.status = 400
-          ctx.response.body = `Задачи с идентификатором ${id} не существует!`;
-          return;
-        }
-        
-        const ticket = tickets.filter(item => item.id === id);
-        ctx.response.body = ticket;
-        ctx.response.status = 202;
-        console.log(ticket);
-        return;
-
-      default:
-          ctx.response.status = 404;
-          return;
+    default:
+      ctx.response.status = 404;
   }
 });
 
@@ -156,17 +169,8 @@ const server = http.createServer(app.callback());
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('Произошла ошибка:', err);
+    return console.error('Произошла ошибка:', err); // eslint-disable-line no-console
   }
-  
-  console.log(`Сервер запущен в ${nowDateTime()}. Прослушиваем порт: ${port}.`);
-});
 
-function nowDateTime() {
-  const now = new Date();
-  const nowDateTime = `${now.toLocaleString([], 
-    {day: 'numeric', month: 'numeric', year: 'numeric',
-     hour: '2-digit', minute: '2-digit', hour12: false })}`;
-     
-  return nowDateTime;
-}
+  return console.log(`Сервер запущен в ${nowDateTime()}. Прослушиваем порт: ${port}.`); // eslint-disable-line no-console
+});
